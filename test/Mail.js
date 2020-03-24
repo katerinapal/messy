aa8a01dd2da9dd9f4015b63c25184d3ddedfb95c
Katerina Paltoglou <katpaltoglou@gmail.com>
2020-03-24 16:39:27 +0200

    update branch

--- test/Mail.js
+++ test/Mail.js
@@ -1,15 +1,14 @@
-/*global describe, it*/
-var expect = require('unexpected'),
-    Mail = require('../lib/Mail');
+import expect from "unexpected";
+import { Mail as libMail_Mailjs } from "../lib/Mail";
 
 describe('Mail', function () {
     it('should rfc2047 decode the header values', function () {
-        var mail = new Mail('Subject: =?iso-8859-1?Q?=A1?=Hola, se=?iso-8859-1?Q?=F1?=or!');
+        var mail = new libMail_Mailjs('Subject: =?iso-8859-1?Q?=A1?=Hola, se=?iso-8859-1?Q?=F1?=or!');
         expect(mail.headers.get('subject'), 'to equal', '¡Hola, señor!');
     });
 
     it('should rfc2047 encode when serializing', function () {
-        var mail = new Mail({body: 'bar'});
+        var mail = new libMail_Mailjs({body: 'bar'});
         mail.headers.set('subject', '¡Hola, señor!');
         expect(mail.toString(), 'to equal', 'Subject: =?utf-8?Q?=C2=A1Hola=2C?= =?utf-8?Q?_se=C3=B1or!?=\r\n\r\nbar');
     });
@@ -17,7 +16,7 @@ describe('Mail', function () {
     describe('#fileName', function () {
         describe('when invoked as a getter', function () {
             it('should fall back to the name property of the Content-Type header when the Content-Disposition header has no filename parameter', function () {
-                var mail = new Mail(
+                var mail = new libMail_Mailjs(
                     'Content-Transfer-Encoding: base64\r\n' +
                     'Content-Disposition: attachment\r\n' +
                     'Content-Type: image/png; name="=?iso-8859-1?Q?=E6=F8=E5.png?="'
@@ -26,7 +25,7 @@ describe('Mail', function () {
             });
 
             it('should fall back to the name property of the Content-Type header when there is no Content-Disposition header', function () {
-                var mail = new Mail(
+                var mail = new libMail_Mailjs(
                     'Content-Transfer-Encoding: base64\r\n' +
                     'Content-Type: image/png; name="=?iso-8859-1?Q?=E6=F8=E5.png?="'
                 );
@@ -36,7 +35,7 @@ describe('Mail', function () {
 
         describe('when invoked as a setter', function () {
             it('should update the name property of the Content-Type header if available', function () {
-                var mail = new Mail({headers: {'Content-Type': 'image/png'}});
+                var mail = new libMail_Mailjs({headers: {'Content-Type': 'image/png'}});
                 mail.fileName = 'æøå.png';
                 expect(
                     mail.toString(),
