7e7c19ddfc6d61e760164337b81ed263078ba6ef
Katerina Paltoglou <katpaltoglou@gmail.com>
2020-03-24 16:32:07 +0200

    update branch

--- test/HttpConversation.js
+++ test/HttpConversation.js
@@ -1,200 +1,180 @@
-import expect from "unexpected";
-import { HttpConversation as libHttpConversation_HttpConversationjs } from "../lib/HttpConversation";
-import { HttpExchange as libHttpExchange_HttpExchangejs } from "../lib/HttpExchange";
-import { HttpRequest as libHttpRequest_HttpRequestjs } from "../lib/HttpRequest";
-import { HttpResponse as libHttpResponse_HttpResponsejs } from "../lib/HttpResponse";
+"use strict";
+
+var _unexpected = require("unexpected");
+
+var _unexpected2 = _interopRequireDefault(_unexpected);
+
+var _HttpConversation = require("../lib/HttpConversation");
+
+var _HttpExchange = require("../lib/HttpExchange");
+
+var _HttpRequest = require("../lib/HttpRequest");
+
+var _HttpResponse = require("../lib/HttpResponse");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 describe('HttpConversation', function () {
     it('should accept an object with an exchanges property containing an array of objects containing an HttpRequest and HttpResponse instances', function () {
-        var httpConversation = new libHttpConversation_HttpConversationjs({
-            exchanges: [
-                {
-                    request: new libHttpRequest_HttpRequestjs('GET / HTTP/1.1\nFoo: Bar\n\nblah'),
-                    response: new libHttpResponse_HttpResponsejs('HTTP/1.1 200 OK\nQuux: Baz\n\nblaf')
-                }
-            ]
+        var httpConversation = new _HttpConversation.HttpConversation({
+            exchanges: [{
+                request: new _HttpRequest.HttpRequest('GET / HTTP/1.1\nFoo: Bar\n\nblah'),
+                response: new _HttpResponse.HttpResponse('HTTP/1.1 200 OK\nQuux: Baz\n\nblaf')
+            }]
         });
 
-        expect(httpConversation.exchanges, 'to have items satisfying', 'to be an', libHttpExchange_HttpExchangejs);
-        expect(
-            httpConversation.toString(),
-            'to equal',
-            'GET / HTTP/1.1\r\nFoo: Bar\r\n\r\nblah\r\n\r\nHTTP/1.1 200 OK\r\nQuux: Baz\r\n\r\nblaf'
-        );
+        (0, _unexpected2.default)(httpConversation.exchanges, 'to have items satisfying', 'to be an', _HttpExchange.HttpExchange);
+        (0, _unexpected2.default)(httpConversation.toString(), 'to equal', 'GET / HTTP/1.1\r\nFoo: Bar\r\n\r\nblah\r\n\r\nHTTP/1.1 200 OK\r\nQuux: Baz\r\n\r\nblaf');
     });
 
     it('should accept an object with an exchanges property containing array of objects containing request and response as strings', function () {
-        var httpConversation = new libHttpConversation_HttpConversationjs({
-            exchanges: [
-                new libHttpExchange_HttpExchangejs({
-                    request: 'GET / HTTP/1.1\nFoo: Bar\n\nblah',
-                    response: 'HTTP/1.1 200 OK\nQuux: Baz\n\nblaf'
-                })
-            ]
+        var httpConversation = new _HttpConversation.HttpConversation({
+            exchanges: [new _HttpExchange.HttpExchange({
+                request: 'GET / HTTP/1.1\nFoo: Bar\n\nblah',
+                response: 'HTTP/1.1 200 OK\nQuux: Baz\n\nblaf'
+            })]
         });
-        expect(
-            httpConversation.toString(),
-            'to equal',
-            'GET / HTTP/1.1\r\nFoo: Bar\r\n\r\nblah\r\n\r\nHTTP/1.1 200 OK\r\nQuux: Baz\r\n\r\nblaf'
-        );
+        (0, _unexpected2.default)(httpConversation.toString(), 'to equal', 'GET / HTTP/1.1\r\nFoo: Bar\r\n\r\nblah\r\n\r\nHTTP/1.1 200 OK\r\nQuux: Baz\r\n\r\nblaf');
     });
 
     it('should accept an object with an exchanges property containing HttpRequest and HttpResponse options objects', function () {
-        var httpConversation = new libHttpConversation_HttpConversationjs({
-            exchanges: [
-                {
-                    request: {
-                        requestLine: {
-                            method: 'GET',
-                            protocol: 'HTTP/1.1',
-                            path: '/'
-                        },
-                        headers: {
-                            'Content-Type': 'text/html'
-                        },
-                        body: 'The Body'
+        var httpConversation = new _HttpConversation.HttpConversation({
+            exchanges: [{
+                request: {
+                    requestLine: {
+                        method: 'GET',
+                        protocol: 'HTTP/1.1',
+                        path: '/'
                     },
-                    response: {
-                        statusLine: 'HTTP/1.1 404 Not Found',
-                        headers: 'Content-Type: application/json',
-                        body: {foo: 123}
-                    }
+                    headers: {
+                        'Content-Type': 'text/html'
+                    },
+                    body: 'The Body'
+                },
+                response: {
+                    statusLine: 'HTTP/1.1 404 Not Found',
+                    headers: 'Content-Type: application/json',
+                    body: { foo: 123 }
                 }
-            ]
+            }]
         });
-        expect(
-            httpConversation.toString(),
-            'to equal',
-            'GET / HTTP/1.1\r\nContent-Type: text/html\r\n\r\nThe Body\r\n\r\nHTTP/1.1 404 Not Found\r\nContent-Type: application/json\r\n\r\n{"foo":123}'
-        );
+        (0, _unexpected2.default)(httpConversation.toString(), 'to equal', 'GET / HTTP/1.1\r\nContent-Type: text/html\r\n\r\nThe Body\r\n\r\nHTTP/1.1 404 Not Found\r\nContent-Type: application/json\r\n\r\n{"foo":123}');
     });
 
     it('should consider identical instances equal', function () {
-        var httpConversation1 = new libHttpConversation_HttpConversationjs({
-            exchanges: [
-                {
-                    request: 'GET /foo HTTP/1.1\r\nHost: foo.com\r\n\r\nblah',
-                    response: {
-                        statusLine: {
-                            statusCode: 200,
-                            protocol: 'HTTP/1.1',
-                            statusMessage: 'OK'
-                        },
-                        body: 'blaf'
-                    }
-                }
-            ]
-        }), httpConversation2 = new libHttpConversation_HttpConversationjs({
-            exchanges: [
-                {
-                    request: {
-                        method: 'GET',
-                        url: '/foo',
+        var httpConversation1 = new _HttpConversation.HttpConversation({
+            exchanges: [{
+                request: 'GET /foo HTTP/1.1\r\nHost: foo.com\r\n\r\nblah',
+                response: {
+                    statusLine: {
+                        statusCode: 200,
                         protocol: 'HTTP/1.1',
-                        headers: {
-                            host: 'foo.com'
-                        },
-                        body: 'blah'
+                        statusMessage: 'OK'
                     },
-                    response: 'HTTP/1.1 200 OK\r\n\r\nblaf'
+                    body: 'blaf'
                 }
-            ]
+            }]
+        }),
+            httpConversation2 = new _HttpConversation.HttpConversation({
+            exchanges: [{
+                request: {
+                    method: 'GET',
+                    url: '/foo',
+                    protocol: 'HTTP/1.1',
+                    headers: {
+                        host: 'foo.com'
+                    },
+                    body: 'blah'
+                },
+                response: 'HTTP/1.1 200 OK\r\n\r\nblaf'
+            }]
         });
-        expect(httpConversation1.equals(httpConversation2), 'to be true');
-        expect(httpConversation1.toString(), 'to equal', httpConversation2.toString());
+        (0, _unexpected2.default)(httpConversation1.equals(httpConversation2), 'to be true');
+        (0, _unexpected2.default)(httpConversation1.toString(), 'to equal', httpConversation2.toString());
     });
 
     it('should consider different instances unequal', function () {
-        var httpConversation1 = new libHttpConversation_HttpConversationjs({
-            exchanges: [
-                {
-                    request: 'GET /foo HTTP/1.0\r\nHost: foo.com\r\n\r\nblah',
-                    response: {
-                        statusLine: {
-                            statusCode: 200,
-                            protocol: 'HTTP/1.1',
-                            statusMessage: 'OK'
-                        },
-                        body: 'blaf'
-                    }
-                }
-            ]
-        }), httpConversation2 = new libHttpConversation_HttpConversationjs({
-            exchanges: [
-                {
-                    request: {
-                        method: 'GET',
-                        url: '/foo',
-                        protocol: 'HTTP/1.1'
+        var httpConversation1 = new _HttpConversation.HttpConversation({
+            exchanges: [{
+                request: 'GET /foo HTTP/1.0\r\nHost: foo.com\r\n\r\nblah',
+                response: {
+                    statusLine: {
+                        statusCode: 200,
+                        protocol: 'HTTP/1.1',
+                        statusMessage: 'OK'
                     },
-                    response: 'HTTP/1.1 200 OK\r\n\r\nblaf'
+                    body: 'blaf'
                 }
-            ]
+            }]
+        }),
+            httpConversation2 = new _HttpConversation.HttpConversation({
+            exchanges: [{
+                request: {
+                    method: 'GET',
+                    url: '/foo',
+                    protocol: 'HTTP/1.1'
+                },
+                response: 'HTTP/1.1 200 OK\r\n\r\nblaf'
+            }]
         });
-        expect(httpConversation1.equals(httpConversation2), 'to be false');
-        expect(httpConversation1.toString(), 'not to equal', httpConversation2.toString());
+        (0, _unexpected2.default)(httpConversation1.equals(httpConversation2), 'to be false');
+        (0, _unexpected2.default)(httpConversation1.toString(), 'not to equal', httpConversation2.toString());
     });
 
     describe('#toJSON', function () {
         it('should return an object with the exchanges JSONified', function () {
-            expect(new libHttpConversation_HttpConversationjs({
-                exchanges: [
-                    {
-                        request: new libHttpRequest_HttpRequestjs('GET / HTTP/1.1\nFoo: Bar\n\nblah'),
-                        response: new libHttpResponse_HttpResponsejs('HTTP/1.1 200 OK\nQuux: Baz\n\nblaf')
-                    },
-                    {
-                        request: new libHttpRequest_HttpRequestjs('GET /foo HTTP/1.1\nFoo: Barrr\n\nblahhh'),
-                        response: new libHttpResponse_HttpResponsejs('HTTP/1.1 412 Precondition Failed\nQuux: Bazzz\n\nblafff')
-                    }
-                ]
+            (0, _unexpected2.default)(new _HttpConversation.HttpConversation({
+                exchanges: [{
+                    request: new _HttpRequest.HttpRequest('GET / HTTP/1.1\nFoo: Bar\n\nblah'),
+                    response: new _HttpResponse.HttpResponse('HTTP/1.1 200 OK\nQuux: Baz\n\nblaf')
+                }, {
+                    request: new _HttpRequest.HttpRequest('GET /foo HTTP/1.1\nFoo: Barrr\n\nblahhh'),
+                    response: new _HttpResponse.HttpResponse('HTTP/1.1 412 Precondition Failed\nQuux: Bazzz\n\nblafff')
+                }]
             }).toJSON(), 'to equal', {
-                exchanges: [
-                    {
-                        request: {
-                            method: 'GET',
-                            url: '/',
-                            protocolName: 'HTTP',
-                            protocolVersion: '1.1',
-                            headers: {
-                                Foo: 'Bar'
-                            },
-                            rawBody: 'blah'
+                exchanges: [{
+                    request: {
+                        method: 'GET',
+                        url: '/',
+                        protocolName: 'HTTP',
+                        protocolVersion: '1.1',
+                        headers: {
+                            Foo: 'Bar'
                         },
-                        response: {
-                            statusCode: 200,
-                            statusMessage: 'OK',
-                            protocolName: 'HTTP',
-                            protocolVersion: '1.1',
-                            headers: {
-                                Quux: 'Baz'
-                            },
-                            rawBody: 'blaf'
-                        }
+                        rawBody: 'blah'
                     },
-                    {
-                        request: {
-                            method: 'GET',
-                            url: '/foo',
-                            protocolName: 'HTTP',
-                            protocolVersion: '1.1',
-                            headers: {
-                                Foo: 'Barrr'
-                            },
-                            rawBody: 'blahhh'
+                    response: {
+                        statusCode: 200,
+                        statusMessage: 'OK',
+                        protocolName: 'HTTP',
+                        protocolVersion: '1.1',
+                        headers: {
+                            Quux: 'Baz'
                         },
-                        response: {
-                            statusCode: 412,
-                            statusMessage: 'Precondition Failed',
-                            protocolName: 'HTTP',
-                            protocolVersion: '1.1',
-                            headers: {
-                                Quux: 'Bazzz'
-                            },
-                            rawBody: 'blafff'
-                        }
+                        rawBody: 'blaf'
                     }
-                ]
+                }, {
+                    request: {
+                        method: 'GET',
+                        url: '/foo',
+                        protocolName: 'HTTP',
+                        protocolVersion: '1.1',
+                        headers: {
+                            Foo: 'Barrr'
+                        },
+                        rawBody: 'blahhh'
+                    },
+                    response: {
+                        statusCode: 412,
+                        statusMessage: 'Precondition Failed',
+                        protocolName: 'HTTP',
+                        protocolVersion: '1.1',
+                        headers: {
+                            Quux: 'Bazzz'
+                        },
+                        rawBody: 'blafff'
+                    }
+                }]
             });
         });
     });
--- test/Mail.js
+++ test/Mail.js
@@ -1,49 +1,45 @@
-import expect from "unexpected";
-import { Mail as libMail_Mailjs } from "../lib/Mail";
+"use strict";
+
+var _unexpected = require("unexpected");
+
+var _unexpected2 = _interopRequireDefault(_unexpected);
+
+var _Mail = require("../lib/Mail");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 describe('Mail', function () {
     it('should rfc2047 decode the header values', function () {
-        var mail = new libMail_Mailjs('Subject: =?iso-8859-1?Q?=A1?=Hola, se=?iso-8859-1?Q?=F1?=or!');
-        expect(mail.headers.get('subject'), 'to equal', '¡Hola, señor!');
+        var mail = new _Mail.Mail('Subject: =?iso-8859-1?Q?=A1?=Hola, se=?iso-8859-1?Q?=F1?=or!');
+        (0, _unexpected2.default)(mail.headers.get('subject'), 'to equal', '¡Hola, señor!');
     });
 
     it('should rfc2047 encode when serializing', function () {
-        var mail = new libMail_Mailjs({body: 'bar'});
+        var mail = new _Mail.Mail({ body: 'bar' });
         mail.headers.set('subject', '¡Hola, señor!');
-        expect(mail.toString(), 'to equal', 'Subject: =?utf-8?Q?=C2=A1Hola=2C?= =?utf-8?Q?_se=C3=B1or!?=\r\n\r\nbar');
+        (0, _unexpected2.default)(mail.toString(), 'to equal', 'Subject: =?utf-8?Q?=C2=A1Hola=2C?= =?utf-8?Q?_se=C3=B1or!?=\r\n\r\nbar');
     });
 
     describe('#fileName', function () {
         describe('when invoked as a getter', function () {
             it('should fall back to the name property of the Content-Type header when the Content-Disposition header has no filename parameter', function () {
-                var mail = new libMail_Mailjs(
-                    'Content-Transfer-Encoding: base64\r\n' +
-                    'Content-Disposition: attachment\r\n' +
-                    'Content-Type: image/png; name="=?iso-8859-1?Q?=E6=F8=E5.png?="'
-                );
-                expect(mail.fileName, 'to equal', 'æøå.png');
+                var mail = new _Mail.Mail('Content-Transfer-Encoding: base64\r\n' + 'Content-Disposition: attachment\r\n' + 'Content-Type: image/png; name="=?iso-8859-1?Q?=E6=F8=E5.png?="');
+                (0, _unexpected2.default)(mail.fileName, 'to equal', 'æøå.png');
             });
 
             it('should fall back to the name property of the Content-Type header when there is no Content-Disposition header', function () {
-                var mail = new libMail_Mailjs(
-                    'Content-Transfer-Encoding: base64\r\n' +
-                    'Content-Type: image/png; name="=?iso-8859-1?Q?=E6=F8=E5.png?="'
-                );
-                expect(mail.fileName, 'to equal', 'æøå.png');
+                var mail = new _Mail.Mail('Content-Transfer-Encoding: base64\r\n' + 'Content-Type: image/png; name="=?iso-8859-1?Q?=E6=F8=E5.png?="');
+                (0, _unexpected2.default)(mail.fileName, 'to equal', 'æøå.png');
             });
         });
 
         describe('when invoked as a setter', function () {
             it('should update the name property of the Content-Type header if available', function () {
-                var mail = new libMail_Mailjs({headers: {'Content-Type': 'image/png'}});
+                var mail = new _Mail.Mail({ headers: { 'Content-Type': 'image/png' } });
                 mail.fileName = 'æøå.png';
-                expect(
-                    mail.toString(),
-                    'to equal',
-                    // TODO: Would be better to emit 'Content-Type: image/png; name="=?iso-8859-1?Q?=E6=F8=E5.png?="\r\n' +
-                    'Content-Type: image/png; =?utf-8?Q?name=3D=C3=A6=C3=B8=C3=A5?=.png\r\n' +
-                    "Content-Disposition: attachment; filename*=iso-8859-1''%E6%F8%E5.png\r\n"
-                );
+                (0, _unexpected2.default)(mail.toString(), 'to equal',
+                // TODO: Would be better to emit 'Content-Type: image/png; name="=?iso-8859-1?Q?=E6=F8=E5.png?="\r\n' +
+                'Content-Type: image/png; =?utf-8?Q?name=3D=C3=A6=C3=B8=C3=A5?=.png\r\n' + "Content-Disposition: attachment; filename*=iso-8859-1''%E6%F8%E5.png\r\n");
             });
         });
     });
