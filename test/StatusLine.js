34112346211ebe8b22c664d61b8a9c8fd0966de0
Katerina Paltoglou <katpaltoglou@gmail.com>
2020-10-16 15:37:28 +0300

    update refactored branch

--- test/StatusLine.js
+++ test/StatusLine.js
@@ -1,11 +1,10 @@
-/*global describe, it*/
-var expect = require('unexpected'),
-    StatusLine = require('../lib/StatusLine');
+import ext_unexpected_expect from "unexpected";
+import { StatusLine as libStatusLine_StatusLinejs } from "../lib/StatusLine";
 
 describe('StatusLine', function () {
     describe('#toJSON', function () {
         it('should return non-computed properties', function () {
-            expect(new StatusLine('HTTP/1.1 200 OK').toJSON(), 'to equal', {
+            ext_unexpected_expect(new libStatusLine_StatusLinejs('HTTP/1.1 200 OK').toJSON(), 'to equal', {
                 statusCode: 200,
                 statusMessage: 'OK',
                 protocolName: 'HTTP',
@@ -16,7 +15,7 @@ describe('StatusLine', function () {
         // Makes it possible to use statusLine.toJSON() as the RHS of a 'to satisfy' assertion in Unexpected
         // where undefined means that the property must not be present:
         it('should not include the keys that have undefined values', function () {
-            expect(new StatusLine('HTTP/1.1').toJSON(), 'not to have key', 'statusCode');
+            ext_unexpected_expect(new libStatusLine_StatusLinejs('HTTP/1.1').toJSON(), 'not to have key', 'statusCode');
         });
     });
 });
